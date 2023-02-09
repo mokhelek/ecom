c9387a27e04015fc236471f72891687851d0f65b
mokhelek <mkatleho191@gmail.com>
2023-02-09 18:59:30 -0500

    UI enhancements

--- shop/static/js/cart.js
+++ shop/static/js/cart.js
@@ -55,3 +55,17 @@ function addCookieItem(productId, action){
 	
 	location.reload()
 }
+
+
+
+function searchBar(){
+	let searchbar = document.getElementById("searchbar")
+
+	if (searchbar.style.display == "none"){
+		searchbar.style.display = "block";
+	}else{
+		searchbar.style.display = "none";
+	}
+
+
+}
